cc853ad6d6ad10f2248fbf4bb3aec5eda8acc981
venkateshsoundar <114813214+venkateshsoundar@users.noreply.github.com>
2022-10-01 18:05:55 +0530

    Add files via upload
    Day1

---
+++ Day1/FirstProgram.java
@@ -0,0 +1,18 @@
+package week1.Day1;
+
+public class FirstProgram {
+	
+	byte pressure=127;
+	short regno=32767;
+	int price=987654321;
+	long mileage=987654321000L;
+	
+	float tankVolume=23245.2567F;
+	double weight=987654321000.987654321000;
+	
+	char seat='L';
+	boolean blueTooth=false;
+	
+
+}
+
