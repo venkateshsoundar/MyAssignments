4a349868bbd87e1f0e0555f4a0dbe5d734ef6a2c
venkateshsoundar <114813214+venkateshsoundar@users.noreply.github.com>
2022-10-02 14:28:33 +0530

    ClassRoom Codes

---
+++ Day21/Bikefrd.java
@@ -0,0 +1,14 @@
+package week1.Day21;
+
+public class Bikefrd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Bike bke2= new Bike();
+		bke2.GetRegNumber();
+		bke2.PrintbikeName();
+
+	}
+
+}
