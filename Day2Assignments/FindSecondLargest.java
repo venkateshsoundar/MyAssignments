6b62be445b62bf3eec5b4cf2b8b57b6f1642a001
venkateshsoundar <114813214+venkateshsoundar@users.noreply.github.com>
2022-10-06 19:14:56 +0530

    Assignment Day2

--- Day2Assignments/FindSecondLargest.java
+++ Day2Assignments/FindSecondLargest.java
@@ -10,8 +10,9 @@ public class FindSecondLargest {
 		int[] data = {3,2,11,4,6,7};
 		
 		Arrays.sort(data);
+			
 		
-		System.out.println(data[1]);
+		System.out.println(data[data.length-2]);
 		
 	}
 
