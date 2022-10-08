d1081ea1289f737b3143fd781472f98a03a20ca1
venkateshsoundar <114813214+venkateshsoundar@users.noreply.github.com>
2022-10-08 19:38:47 +0530

    Week2 Day 1 Assignments

---
+++ week2/Day1/Assignmentwo.java
@@ -0,0 +1,80 @@
+package week2.Day1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Assignmentwo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+WebDriverManager.chromedriver().setup(); 
+		
+		ChromeDriver driver=new ChromeDriver();
+		
+		driver.get("http://leaftaps.com/opentaps/control/main");
+		
+		driver.manage().window().maximize();
+		
+		WebElement Username = driver.findElement(By.id("username"));
+		Username.sendKeys("Demosalesmanager");
+		
+		WebElement Password = driver.findElement(By.id("password"));
+		Password.sendKeys("crmsfa");
+		
+		WebElement login = driver.findElement(By.className("decorativeSubmit"));
+		login.click();
+		
+		WebElement link = driver.findElement(By.linkText("CRM/SFA"));
+		
+		link.click();
+		
+		WebElement Leads = driver.findElement(By.linkText("Leads"));
+		Leads.click();
+		
+		driver.findElement(By.linkText("Create Lead")).click();
+	
+		WebElement Companyname = driver.findElement(By.id("createLeadForm_companyName"));
+		Companyname.sendKeys("CTS");
+		
+		WebElement FirstName = driver.findElement(By.id("createLeadForm_firstName"));
+		FirstName.sendKeys("Venkat");
+		
+		WebElement LastName = driver.findElement(By.id("createLeadForm_lastName"));
+		LastName.sendKeys("Soundar");
+		
+		WebElement FirstnameLoc = driver.findElement(By.id("createLeadForm_firstNameLocal"));
+		FirstnameLoc.sendKeys("Venki");
+		
+		WebElement Department = driver.findElement(By.id("createLeadForm_departmentName"));
+		Department.sendKeys("Testing");
+		
+		WebElement Description = driver.findElement(By.id("createLeadForm_description"));
+		Description.sendKeys("Learning Selenium");
+		
+		WebElement Email = driver.findElement(By.id("createLeadForm_primaryEmail"));
+		Email.sendKeys("venkimech1992@gmail.com");
+		
+		WebElement CreateLead = driver.findElement(By.className("smallSubmit"));
+		CreateLead.click();
+		
+		WebElement Edit = driver.findElement(By.linkText("Edit"));
+		Edit.click();
+		
+		WebElement UpdDescription = driver.findElement(By.id("updateLeadForm_description"));
+		UpdDescription.clear();
+		
+		WebElement impNote = driver.findElement(By.id("updateLeadForm_importantNote"));
+		impNote.sendKeys("Assignment");
+		
+		WebElement Update = driver.findElement(By.className("smallSubmit"));
+		Update.click();
+		
+		String title = driver.getTitle();
+		System.out.println(title);
+		
+	}
+
+}
